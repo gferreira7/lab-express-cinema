59d27f68fcf796ac9007d69e7a653f3037b96c06
gferreira7 <104030930+gferreira7@users.noreply.github.com>
2023-02-16 20:43:15 +0000

    finishing lab minus bonus

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,29 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express')
+const MoviesModel = require('../models/Movies.model')
+const router = express.Router()
 
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get('/', (req, res, next) => res.render('index'))
+router.get('/movies', async (req, res) => {
+  try {
+    let foundMovies = await MoviesModel.find()
+    res.render('movies', { foundMovies })
+  } catch (error) {
+    console.log(error)
+  }
+})
 
-module.exports = router;
+router.get('/movies/:id', async (req, res) => {
+  try {
+    const { movieId } = req.params.id
+    
+    let foundMovie = await MoviesModel.findById(req.params.id)
+    console.log(foundMovie)
+    res.render('movie-detail', foundMovie)
+
+  } catch (error) {
+    console.log(error)
+  }
+})
+
+module.exports = router
